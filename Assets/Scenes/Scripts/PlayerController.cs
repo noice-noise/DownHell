1b70ecee31a2bb3a50c2763f899e56fad45ced72
Raul Comeros, Jr <redqweqwecode001@gmail.com>
2021-11-29 20:34:45 +0800

    Initial commit

---
+++ Assets/Scenes/Scripts/PlayerController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+using static UnityEngine.UI.Scrollbar;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    
+    [SerializeField]
+    private float jumpForce = 10f;
+
+    [SerializeField]
+    private float sideForce = 20f;
+    private Vector3 direction;
+    public bool keyrightPressed;
+    private bool keyleftPressed;
+    private bool jumpingKeyheld;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        HandleInputs();
+        HandleJumping();
+    }
+
+    private void HandleJumping()
+    {
+        if (jumpingKeyheld) {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private void HandleInputs()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            jumpingKeyheld = true;
+        } else {
+            jumpingKeyheld = false;
+        }
+
+        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+            
+        }
+
+        if (Input.GetKeyDown(KeyCode.RightArrow)) {
+            direction = Vector3.right;
+        }
+
+        rb.AddForce(direction * sideForce, ForceMode.Acceleration);
+    }
+}
